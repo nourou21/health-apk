56164f9900e56668f85efc81c515c1964d6b8c9d
nourou21 <qabdenourouhba@gmail.com>
2024-10-18 00:42:44 +0100

    nourou first try

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/constants.dart';
+import 'package:flutter_application_1/welcome/welcome_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Healthcare - Doctor Consultation App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: primaryColor,
+        textTheme: Theme.of(context).textTheme.apply(displayColor: textColor),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: TextButton.styleFrom(
+            backgroundColor: primaryColor,
+            padding: EdgeInsets.all(defaultPadding),
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          border: textFieldBorder,
+          enabledBorder: textFieldBorder,
+          focusedBorder: textFieldBorder,
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
